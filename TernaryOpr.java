a713104d3efc5c3cb4d7a9308cb283a4bac7b658
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-03-10 06:57:37 +0530

    Add files via upload

---
+++ TernaryOpr.java
@@ -0,0 +1,10 @@
+public class TernaryOpr {
+    public static void main(String[] args){
+        int a=19,b=20, num, big;
+        num= (a>=10)? a : 10;
+        System.out.println(num);
+        big= (a>b) ? a : b;
+        System.out.println(big);
+
+    }
+}
