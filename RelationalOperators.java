357d8b1a76d7d6c5fba3b97a026a718d8662a502
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-03-10 06:51:50 +0530

    Add files via upload

---
+++ RelationalOperators.java
@@ -0,0 +1,16 @@
+public class RelationalOperators {
+    public static void main(String[] args)
+    {
+        float a= 15.0f, b=20.75f, c= 15.0f;
+
+        System.out.println(" a < b is " + (a<b));
+        System.out.println(" a > b is " + (a>b));
+        System.out.println(" a == c is " + (a==c));
+        System.out.println(" a <= c is " + (a<=c));
+        System.out.println(" a >= b is " + (a>=b));
+        System.out.println(" a != c is " + (a!=b));
+        System.out.println(" b!= c is " + (b!=c));
+        System.out.println(" b == a+c is " + (b==a+c));
+
+    }
+}
