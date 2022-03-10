80ecc75684a07b5b4598156220405052b3ca032a
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-03-10 06:54:24 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        //To print numbers from 1 to 10
+        int number = 1;
+        while (number <= 10) {
+            System.out.print(number + " ");
+            number++;
+        }
+    }
+}
