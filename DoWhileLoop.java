4b7a4d23bf0629cf4efd0d14cc43160d3ac6929e
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-03-10 06:47:58 +0530

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,12 @@
+public class DoWhileLoop {
+    public static void main(String[] args)
+    {
+        int i=0;
+        do
+        {
+            System.out.print(i + " ");
+            i++;
+        } while ((i>=1) && (i<=10));
+        //prints the initial element before checking the while condition
+    }
+}
